0f8426aaf9a7f109750922d1c75129c5346bcf78
KayoGustavo <154030421+KayoGustavo@users.noreply.github.com>
2026-04-03 21:48:49 -0300

    commit teste

--- public/index.php
+++ public/index.php
@@ -6,6 +6,8 @@ $usuarios = [
     ['usuario' => 'carlos', 'senha' => '1234', 'tipo' => 'cozinha'],
 ];
 
+//teste push
+
 $erro = '';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -56,5 +58,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         </form>
 
     </div>
-
+    
 <?php include 'partials/footer.php'; ?>
